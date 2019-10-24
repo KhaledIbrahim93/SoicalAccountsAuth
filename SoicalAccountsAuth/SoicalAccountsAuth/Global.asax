d9a4dedef16e6fa0d489ca0ebb46701d4a18f679
Khaled Ibrahim <khaledcs@outlook.com>
2019-10-24 12:45:17 +0200

    FirstCommit

---
+++ SoicalAccountsAuth/SoicalAccountsAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoicalAccountsAuth.MvcApplication" Language="C#" %>
---
+++ SoicalAccountsAuth/SoicalAccountsAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoicalAccountsAuth.Startup))]
+namespace SoicalAccountsAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
